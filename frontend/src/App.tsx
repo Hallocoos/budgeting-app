bc2d42fba974fb92e24152504eae3231c95bc75c
RWEngelbrecht <rigardte@gmail.com>
2021-06-17 12:28:13 +0200

    added navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
-import Login from './components/login/login';
+import Login from './components/login';
 import Home from './pages/home/home';
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
         <Router>
           <div className="App">
-            <header className="App-header">
-              YNABB
-              {/* <Nav /> */}
+            <header data-testid="header" className="App-header">
+              <Navbar title="YNABB" />
             </header>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
